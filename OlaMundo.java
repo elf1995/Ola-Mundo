42f287b7fe1d28e4d61cc0fae79863c42b97a2f7
elf1995 <89623278+elf1995@users.noreply.github.com>
2021-08-27 01:10:24 -0300

    Um pequeno programa em Java
    Pequeno programa em java que exibe na tela "Olá Mundo!"

---
+++ OlaMundo.java
@@ -0,0 +1,11 @@
+/* Autor: elf1995
+   Data: 27/08/2021
+   Finalidade: Exibir no terminal a mensagem "Olá Mundo"
+*/
+
+Public class OlaMundo { // Declaro a classe Ola Mundo com o modificador de acesso Public
+  // Inicia a o método principal
+  public static void main(String[] args) {
+    System.out.println("Olá Mundo!"); // Imprime na tela Olá Mundo!
+  }
+}
